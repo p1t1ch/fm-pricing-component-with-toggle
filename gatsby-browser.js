f52989882c843108152461cb9ea4ead3045960a6
p1t1ch <belobeev.kirill@gmail.com>
2021-02-02 11:58:55 +0300

    setup theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 import 'focus-visible'
+import '@fontsource/montserrat/latin-700.css'
 import '@/styles/global.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,40 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      neutral: {
+        'blue-grayish': '#6E728E',
+        'blue-grayish-dark': '#4A4D60',
+        blue: '#6D72DE',
+        'blue-light': '#D0D5F6',
+        'blue-lightest': '#F7F7FE',
+      },
+      gradient: {
+        start: '#A2A7F0',
+        end: '#696EDD',
+      },
+      white: '#FFF',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        body: ['.9375rem', 1.87],
+      },
+      boxShadow: {
+        DEFAULT: '0px 20px 40px rgba(212, 210, 244, 0.5)',
+      },
+    },
   },
 }
+
+// ### Primary
+
+// - Linear Gradient: hsl(236, 72%, 79%) to hsl(237, 63%, 64%)
+
+// ### Neutral
+
+// - Very Light Grayish Blue: hsl(240, 78%, 98%)
+// - Light Grayish Blue: hsl(234, 14%, 74%)
+// - Grayish Blue: hsl(233, 13%, 49%)
